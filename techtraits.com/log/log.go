cecf18df5258d89ab562788fec162cd5101ba5ae
usmanismail <0xfffffff@gmail.com>
2013-09-02 18:37:58 -0700

    Adding support for parameters in the log wrapper

--- techtraits.com/log/log.go
+++ techtraits.com/log/log.go
@@ -4,18 +4,34 @@ import (
    	"log"
 )
 
-func Info(logMessage string) {
-	log.Printf("[INFO] "+logMessage) 
+func Info(logMessage string, v ...interface{}) {
+	if v != nil {
+		log.Printf("[INFO]  "+ logMessage, v)
+	} else {
+		log.Printf("[INFO]  "+ logMessage)
+	} 
 }
 
-func Debug(logMessage string) {
-	log.Printf("[DEBUG] "+logMessage) 
+func Debug(logMessage string, v ...interface{}) {
+	if v != nil {
+		log.Printf("[DEBUG]  "+ logMessage, v)
+	} else {
+		log.Printf("[DEBUG]  "+ logMessage)
+	}
 }
 
-func Warn(logMessage string) {
-	log.Printf("[WARN] "+logMessage) 
+func Warn(logMessage string, v ...interface{}) {
+	if v != nil {
+		log.Printf("[WARN]  "+ logMessage, v)
+	} else {
+		log.Printf("[WARN]  "+ logMessage)
+	} 
 }
 
-func Error(logMessage string) {
-	log.Printf("[ERROR] "+logMessage) 
+func Error(logMessage string, v ...interface{}) {
+	if v != nil {
+		log.Printf("[ERROR]  "+ logMessage, v)
+	} else {
+		log.Printf("[ERROR]  "+ logMessage)
+	} 
 }
