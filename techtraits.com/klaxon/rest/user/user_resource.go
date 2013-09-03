c2449656f2960a6fb32f0955babff443da2793c7
usmanismail <0xfffffff@gmail.com>
2013-09-02 18:38:42 -0700

    Adding seperate handlers for Get User, Get Users and Post User

--- techtraits.com/klaxon/rest/user/user_resource.go
+++ techtraits.com/klaxon/rest/user/user_resource.go
@@ -4,16 +4,27 @@ import (
 	"techtraits.com/log"
 	"techtraits.com/klaxon/rest/router"
 	"net/http"
+	"net/url"
 )
 
 
 func init() {
-	log.Debug("Initilizing User Resource")
-
-    router.Register("/user/{user_id}/{transaction}", router.GET, nil,nil,  getUsers)
+	router.Register("/user/", router.GET, []string{"application/json"} , nil,  getUsers)
+    router.Register("/user/{user_id}/", router.GET, []string{"application/json"} , nil,  getUser)
+    router.Register("/user/", router.POST, []string{"application/json"} , nil,  postUser)
 }
 
-func getUsers (route router.Route, pathParams map[string]string, queryParams map[string]string,headers http.Header) {
+func getUsers (route router.Route, pathParams map[string]string, queryParams url.Values,headers http.Header) {
 
-	log.Info("Callback called");
+	log.Info("Get Users");
+}
+
+func postUser (route router.Route, pathParams map[string]string, queryParams url.Values,headers http.Header) {
+
+	log.Info("Post User");
+}
+
+func getUser (route router.Route, pathParams map[string]string, queryParams url.Values,headers http.Header) {
+
+	log.Info("Get User");
 }
