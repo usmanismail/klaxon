dfb9b5cb431b27baf2d6e0b0ac296a26e2d4cb93
usmanismail <0xfffffff@gmail.com>
2013-09-28 15:33:25 -0700

    Added support for looking up values from graphite

---
+++ techtraits.com/graphite/graphite_reader.go
@@ -0,0 +1,60 @@
+package graphite
+
+import (
+	"appengine"
+	"appengine/urlfetch"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+type GraphiteReader interface {
+	ReadValue(target string) (float64, error)
+}
+
+type GraphiteHandler struct {
+	BaseUrl        *url.URL
+	LookBack       string
+	GraphiteClient *http.Client
+}
+
+func MakeGraphiteReader(baseUrlStr string, context appengine.Context) (GraphiteReader, error) {
+
+	baseUrl, err := url.Parse(baseUrlStr + "/render")
+
+	var reader GraphiteHandler
+	if err == nil {
+		reader = GraphiteHandler{baseUrl, "-300sec", urlfetch.Client(context)}
+	}
+
+	return reader, err
+}
+
+func (this GraphiteHandler) ReadValue(target string) (float64, error) {
+
+	var value float64
+	graphiteUrl, err := url.Parse(this.BaseUrl.String() + "?target=" + target + "&format=csv&from=" + this.LookBack)
+
+	if err == nil {
+		var resp *http.Response
+		resp, err = this.GraphiteClient.Get(graphiteUrl.String())
+		if err == nil {
+			defer resp.Body.Close()
+			var content []byte
+			content, err = ioutil.ReadAll(resp.Body)
+			recordStrings := strings.Split(strings.TrimSpace(string(content[:])), "\n")
+			err = errors.New("no-known-value")
+			for _, recordString := range recordStrings {
+				records := strings.Split(strings.TrimSpace(recordString), ",")
+				if len(records) == 3 && len(records[2]) != 0 {
+					err = nil
+					value, _ = strconv.ParseFloat(records[2], 64)
+				}
+			}
+		}
+	}
+	return value, err
+}
--- techtraits.com/klaxon/rest/internal/check/check_resource.go
+++ techtraits.com/klaxon/rest/internal/check/check_resource.go
@@ -2,16 +2,17 @@ package check
 
 import (
 	"net/http"
+	"techtraits.com/graphite"
 	"techtraits.com/klaxon/rest/alert"
 	"techtraits.com/klaxon/router"
 	"techtraits.com/log"
 )
 
 func init() {
-	router.Register("/rest/internal/check/{project_id}", router.POST, nil, nil, getTick)
+	router.Register("/rest/internal/check/{project_id}", router.POST, nil, nil, checkProject)
 }
 
-func getTick(request router.Request) (int, []byte) {
+func checkProject(request router.Request) (int, []byte) {
 
 	//Check that Project exists
 
@@ -22,6 +23,11 @@ func getTick(request router.Request) (int, []byte) {
 		return http.StatusInternalServerError, []byte(err.Error())
 	} else {
 		log.Infof(request.GetContext(), "Got alerts %v", alerts)
+		graphiteReader, _ := graphite.MakeGraphiteReader("https://www.hostedgraphite.com/0c90142e/3f0f94a7-376b-4562-b174-ee3e769813b3/graphite", request.GetContext())
+		for _, alert := range alerts {
+			value, err := graphiteReader.ReadValue(alert.Target)
+			log.Info("response %v", value, err)
+		}
 
 	}
 
