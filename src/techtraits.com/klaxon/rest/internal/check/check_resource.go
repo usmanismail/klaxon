7612e3f8e2cb575b90572eb4781ede835183b2ef
usmanismail <0xfffffff@gmail.com>
2013-10-12 13:59:16 -0700

    Sending email when alert is triggered using subscriptions, first implementation

--- src/techtraits.com/klaxon/rest/internal/check/check_resource.go
+++ src/techtraits.com/klaxon/rest/internal/check/check_resource.go
@@ -2,6 +2,7 @@ package check
 
 import (
 	"appengine"
+	"appengine/mail"
 	"encoding/json"
 	"errors"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"techtraits.com/graphite"
 	"techtraits.com/klaxon/rest/alert"
 	"techtraits.com/klaxon/rest/project"
+	"techtraits.com/klaxon/rest/subscription"
 	"techtraits.com/klaxon/router"
 	"techtraits.com/log"
 )
@@ -17,28 +19,44 @@ func init() {
 	router.Register("/rest/internal/check/{project_id}", router.POST, nil, nil, checkProject)
 }
 
+//TODO: Check if project is enabled
 func checkProject(request router.Request) (int, []byte) {
 
-	//Check that Project exists
-	projectDto, err := project.GetProjectDTOFromGAE(request.GetPathParams()["project_id"], request.GetContext())
+	//Get The project
+	projectId := request.GetPathParams()["project_id"]
+	projectObj, getProjectStatus, getProjectError := getProject(projectId, request.GetContext())
+	//Get Alerts
+	alerts, getAlertStatus, getAlertsError := getAlerts(projectId, request.GetContext())
+	//Get Subscriptions
+	subscriptions, getSubsStatus, getSubsError := getSubscriptions(projectId, request.GetContext())
 
-	//TODO: Check if project is enabled
-	if err != nil && strings.Contains(err.Error(), "no such entity") {
-		log.Errorf(request.GetContext(), "Error retriving project: %v", err)
-		return http.StatusNotFound, []byte(err.Error())
-	} else if err != nil {
-		log.Errorf(request.GetContext(), "Error retriving project: %v", err)
-		return http.StatusInternalServerError, []byte(err.Error())
+	if getProjectError == nil && getAlertsError == nil && getSubsError == nil {
+		//Check Alerts
+		return processAlerts(projectObj, alerts, subscriptions, request.GetContext())
 	} else {
-		projectObj, err := projectDto.GetProject()
-		status, err := verifyProject(projectObj, err, request.GetContext())
-		if err == nil {
-			return processAlerts(projectObj, request.GetContext())
-		} else {
-			return status, []byte(err.Error())
-		}
+		return processError(getProjectStatus, getAlertStatus, getSubsStatus,
+			getProjectError, getAlertsError, getSubsError)
 	}
 
+}
+
+func getProject(projectId string, context appengine.Context) (project.Project, int, error) {
+	projectDto, err := project.GetProjectDTOFromGAE(projectId, context)
+	if err != nil && strings.Contains(err.Error(), "no such entity") {
+		log.Errorf(context, "Error retriving project: %v", err)
+		return nil, http.StatusNotFound, err
+	} else if err != nil {
+		log.Errorf(context, "Error retriving project: %v", err)
+		return nil, http.StatusInternalServerError, err
+	} else {
+		projectObj, err := projectDto.GetProject()
+		status, err := verifyProject(projectObj, err, context)
+		if err != nil {
+			return nil, status, err
+		} else {
+			return projectObj, 0, nil
+		}
+	}
 }
 
 func verifyProject(projectObj project.Project, err error, context appengine.Context) (int, error) {
@@ -56,35 +74,56 @@ func verifyProject(projectObj project.Project, err error, context appengine.Cont
 	}
 }
 
-func processAlerts(projectObj project.Project, context appengine.Context) (int, []byte) {
-
+func getAlerts(projectId string, context appengine.Context) ([]alert.Alert, int, error) {
 	//Get Alerts for Project
-	alerts, err := alert.GetAlertsFromGAE(projectObj.GetName(), context)
+	alerts, err := alert.GetAlertsFromGAE(projectId, context)
 	if err != nil {
 		log.Errorf(context, "Error retriving alerts: %v", err)
-		return http.StatusInternalServerError, []byte(err.Error())
+		return nil, http.StatusInternalServerError, err
 	} else {
-		alertChecks := make([]alert.Check, 0)
-		graphiteReader, _ := graphite.MakeGraphiteReader(projectObj.GetConfig()["graphite.baseurl"],
-			projectObj.GetConfig()["graphite.lookback"], context)
-
-		for _, projectAlert := range alerts {
-			alertCheck := processAlert(projectAlert, context, graphiteReader, alertChecks)
-			alertChecks = append(alertChecks, alertCheck)
-		}
-
-		alertBytes, err := json.MarshalIndent(alertChecks, "", "	")
-
-		if err != nil {
-			log.Errorf(context, "Error marshalling response %v", err)
-			return http.StatusInternalServerError, []byte(err.Error())
-		}
-		return http.StatusOK, alertBytes
+		return alerts, http.StatusOK, nil
 	}
 }
 
+func getSubscriptions(projectId string, context appengine.Context) ([]subscription.Subscription, int, error) {
+	//Get Subscriptions for Project
+	subscriptions, err := subscription.GetSubscriptionsFromGAE(projectId, context)
+	if err != nil {
+		log.Errorf(context, "Error retriving subscriptions: %v", err)
+		return nil, http.StatusInternalServerError, err
+	} else {
+		return subscriptions, http.StatusOK, nil
+	}
+}
+
+func processAlerts(projectObj project.Project, alerts []alert.Alert,
+	subscriptions []subscription.Subscription, context appengine.Context) (int, []byte) {
+
+	graphiteReader, _ := graphite.MakeGraphiteReader(projectObj.GetConfig()["graphite.baseurl"],
+		projectObj.GetConfig()["graphite.lookback"], context)
+
+	alertChecks := make([]alert.Check, 0)
+
+	for _, projectAlert := range alerts {
+		//TODO Make this in to a go routine
+		alertCheck := processAlert(projectAlert, context, graphiteReader, subscriptions, alertChecks)
+		alertChecks = append(alertChecks, alertCheck)
+	}
+
+	alertBytes, err := json.MarshalIndent(alertChecks, "", "	")
+
+	if err != nil {
+		log.Errorf(context, "Error marshalling response %v", err)
+		return http.StatusInternalServerError, []byte(err.Error())
+	} else {
+		return http.StatusOK, alertBytes
+	}
+
+}
+
 func processAlert(projectAlert alert.Alert, context appengine.Context,
-	graphiteReader graphite.GraphiteReader, alertChecks []alert.Check) alert.Check {
+	graphiteReader graphite.GraphiteReader, subscriptions []subscription.Subscription,
+	alertChecks []alert.Check) alert.Check {
 
 	value, err := graphiteReader.ReadValue(projectAlert.Target)
 	if err != nil {
@@ -96,7 +135,9 @@ func processAlert(projectAlert alert.Alert, context appengine.Context,
 	} else {
 		changed, previous, current := projectAlert.CheckAlertStatusChange(value)
 		saveChangeIfNeeded(projectAlert, changed, current, context)
-		return alert.Check{projectAlert.Project, projectAlert.Name, previous, current, changed, value}
+		check := alert.Check{projectAlert.Project, projectAlert.Name, previous, current, changed, value}
+		triggerSubscriptionsIfNeeded(check, subscriptions, context)
+		return check
 	}
 }
 
@@ -104,5 +145,38 @@ func saveChangeIfNeeded(projectAlert alert.Alert, changed bool, current alert.AL
 	if changed {
 		projectAlert.PreviousState = current
 		alert.SaveAlertToGAE(projectAlert, context)
+	}
+}
+
+func triggerSubscriptionsIfNeeded(check alert.Check, subscriptions []subscription.Subscription,
+	context appengine.Context) {
+	for _, subscription := range subscriptions {
+		log.Infof(context, "Firing Subscrption for check %v", subscription, check)
+
+		//url := createConfirmationURL(r)
+		msg := &mail.Message{
+			Sender:  "Klaxon <0xfffffff@gmail.com>",
+			To:      []string{subscription.Target},
+			Subject: "Alert Triggered",
+			Body:    "Alert Triggered",
+		}
+		if err := mail.Send(context, msg); err != nil {
+			log.Errorf(context, "Couldn't send email: %v", err)
+		}
+
+	}
+}
+
+func processError(getProjectStatus int, getAlertStatus int, getSubsStatus int,
+	getProjectError error, getAlertsError error, getSubsError error) (int, []byte) {
+
+	if getProjectError != nil {
+		return getProjectStatus, []byte(getProjectError.Error())
+	} else if getAlertsError != nil {
+		return getAlertStatus, []byte(getAlertsError.Error())
+	} else if getSubsError != nil {
+		return getSubsStatus, []byte(getSubsError.Error())
+	} else {
+		return http.StatusInternalServerError, []byte("Unknown server error")
 	}
 }
